169fabccb8d0ac2dcfd64d273c88d77e3dc835ee
Ali Moghimi <mghmiali@gmail.com>
2025-01-06 08:43:25 +0000

    fix: add request settings env variables

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,14 @@
 from decouple import config
 
 LOG_LEVEL = config("LOG_LEVEL", default="INFO")
-TOKEN = config("TOKEN", default="INFO")
-EXCHANGES_OUTPUT_TABLE = config("OUTPUT_TABLE")
-TICKERS_OUTPUT_TABLE = config("OUTPUT_TABLE")
+TOKEN = config("TOKEN")
+EXCHANGES_OUTPUT_TABLE = config("EXCHANGES_OUTPUT_TABLE")
+TICKERS_OUTPUT_TABLE = config("TICKERS_OUTPUT_TABLE")
 INSERTER_MAX_RETRIES = config("INSERTER_MAX_RETRIES", default=3, cast=int)
-MSSQL_SERVER = config("MSSQL_SERVER")
+REQUEST_MAX_RETRIES = config("REQUEST_MAX_RETRIES", default=3, cast=int)
+REQUEST_BACKOFF_FACTOR = config("REQUEST_BACKOFF_FACTOR", default=2, cast=int)
 MSSQL_AD_LOGIN = config("MSSQL_AD_LOGIN", cast=bool, default=False)
+MSSQL_SERVER = config("MSSQL_SERVER")
 MSSQL_DATABASE = config("MSSQL_DATABASE")
 
 if not MSSQL_AD_LOGIN:
